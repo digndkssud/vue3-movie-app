372842023f9a5ea59ef226b9c0ecbb7ce4631330
digndkssud <digndkssud@gmail.com>
2021-06-21 17:28:30 +0900

    crate project

---
+++ src/components/Logo.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- a 태그와 같은 역할 -->
+  <RouterLink
+    to="/"
+    class="logo">
+    <span>SAKURaAPI</span> .COM
+  </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+
+.logo {
+   font-family: "Oswald", sans-serif;
+   font-size: 20px;
+   color: $black;
+   text-decoration: none;
+   &:hover {
+     color: $black; 
+   }
+   span {
+     color: $primary;
+   }
+}
+</style>
